ce611e7038b7f74d02bafc2275e7f218bc725fce
willber10 <willberschickler@gamil.com>
2024-01-15 14:18:44 -0800

    added fucntions to render the circle

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ let square = new Square();
 let triangle = new Triangle();
 
 console.log(circle.render());
+
+function renderSvg() {
+    return `<svg width="100" height="100">
+    ${circle.render()}
+    </svg>`;
+}
+
+fs.writeFile("examples/circle.svg", renderSvg(), function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("The file was saved!");
+  }
+});
