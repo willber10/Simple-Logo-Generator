b6893908a93342e9d9563db00b54d25966a7a47e
willber10 <willberschickler@gamil.com>
2024-01-15 14:53:23 -0800

    updated index to display questions on which shape and color you would like, updated package.json to go back to a later version of inquirer

--- index.js
+++ index.js
@@ -4,22 +4,57 @@ const fs = require('fs');
 
 const inquirer = require('inquirer');
 
-let circle = new Circle("red");
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'What shape would you like to draw?',
+    choices: ['circle', 'square', 'triangle']
+  },
+  {
+    type: 'input',
+    name: 'color',
+    message: 'What color would you like to use?'
+  }
+]).then(answers => {
+  let shape;
+  switch (answers.shape) {
+    case 'circle':
+      shape = new Circle(answers.color);
+      break;
+    case 'square':
+      shape = new Square(answers.color);
+      break;
+    case 'triangle':
+      shape = new Triangle(answers.color);
+      break;
+  }
+  fs.writeFile("examples/" + answers.shape + ".svg", renderSvg(shape), function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+  })
+});
+
+
+let circle = new Circle("orange");
 let square = new Square();
 let triangle = new Triangle();
 
-console.log(circle.render());
 
-function renderSvg() {
+function renderSvg(shape) {
     return `<svg width="100" height="100">
-    ${circle.render()}
+    ${shape.render()}
     </svg>`;
 }
 
-fs.writeFile("examples/circle.svg", renderSvg(), function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("The file was saved!");
-  }
-});
+// console.log(circle.render());
+// fs.writeFile("examples/circle.svg", renderSvg(circle), function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("The file was saved!");
+//   }
+// });
